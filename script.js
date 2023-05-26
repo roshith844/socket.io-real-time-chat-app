4ae6af1c5cdb83f606cabd5b9c820b083e05254c
ROSHITH P <roshith844@gmail.com>
2023-05-26 21:19:35 +0530

    style: chat box, background

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById('message-container')
+const sendButton = document.getElementById('send-button')
+const userName = "sasi"
+//  prompt('Enter Your Name')
+appendMessage('You Joined')
+socket.emit('new-user', userName)
 
-socket.on('chat-message', data =>{
+socket.on('chat-message', data => {
     console.log(data)
+    appendMessage(`${data.name}: ${data.message}`)
 })
+
+socket.on('user-connected', userName => {
+    appendMessage(`${userName} joined The room`)
+})
+
+
+socket.on('user-disconnected', userName => {
+    appendMessage(`${userName} has Left The room`)
+})
+
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+    scrollToBottom()
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
+function scrollToBottom() {
+    setTimeout(() => {
+        const lastMessage = messageContainer.lastElementChild;
+        lastMessage.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }, 0);
+}
--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const io = require('socket.io')(3000, {
     }
 })
 
+const users = {}
+
 io.on('connection', socket => {
-    socket.emit('chat-message', "Hello world")
+    console.log("new user")
+    socket.on('new-user', userName => {
+        users[socket.id] = userName
+        socket.broadcast.emit('user-connected', userName)
+    })
+
+    socket.on('send-chat-message', message => {
+        console.log(message)
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+
+
 })
+
